8bdc93db5b801b88fee4348036d2bfb47d507520
Vlad <kriptovlad@gmail.com>
2023-04-21 19:30:30 +0300

    v1.0

---
+++ for_dictionary.py
@@ -0,0 +1,15 @@
+import json
+
+normal_words = []
+
+words = open('russian.txt', encoding='windows-1251').read().split()
+for i in words[12:]:
+    if len(i) == 5:
+        normal_words.append(str(i).lower())
+
+with open("data_file.json", "w") as write_file:
+    json.dump(normal_words, write_file, ensure_ascii=False)
+
+with open("data_file.json", "r") as write_file:
+    words = json.load(write_file)
+    print(words)
---
+++ main.py
@@ -0,0 +1,43 @@
+import json
+
+from random import choice
+
+with open("data_file.json", "r") as write_file:
+    words = json.load(write_file)
+
+# word = choice(words)
+word = 'соска'
+print(word)
+
+end = False
+attempts = 0
+
+while not end:
+    input_word = input()
+    if len(input_word) == 5 and input_word in words:
+
+        location = []
+
+        for i in range(len(input_word)):
+            letter = input_word[i]
+
+            if letter in word and word[i] == input_word[i]:
+                location.append(2)
+
+            elif letter in word:
+                location.append(1)
+
+            else:
+                location.append(0)
+
+        print(location)
+
+        if sum(location) == 10:
+            end = True
+
+    elif len(input_word) != 5:
+        print('Введите слово из пяти букв!')
+
+    elif input_word not in words:
+        print('Такого слова не существует')
+print('Победа!')
