77a65bc184c283ac9994afe68a450a9e569573f7
Ilya <mihailovily@tuta.io>
2023-05-07 14:27:21 +0300

    try to create user in chat

--- main_telegram.py
+++ main_telegram.py
@@ -4,11 +4,13 @@ from gamelogic import logic as gmlck
 from work_with_db import changes as db_chng
 from work_with_db import creates as db_crt
 from work_with_db import requests as db_req
+from data import db_session
 
 # в settings/token.txt храним токен в settings/admin.txt - id админа (админов)
 token = open('settings/token.txt').readline().split()[0]
 bot = telebot.TeleBot(token)  # инит
 admins = open('settings/admin.txt').read().split() # админы
+db_session.global_init('db/base.db')
 
 
 @bot.message_handler(commands=['start', 'help'])  # старт
@@ -23,9 +25,18 @@ def send_welcome(message):
 @bot.message_handler(commands=['play'])
 @bot.message_handler(regexp="Поиграть")
 def find(message):
-    bot.reply_to(message, 'Я загадал, угадывай')
     usr_id = str(message.from_user.id)
     word = gmlck.getword()
+    if (db_req.req_user(usr_id)):
+        print('Уже есть')
+    else:
+        db_crt.create_user(usr_id)
+        db_chng.change_status(usr_id, True)
+        db_chng.change_attempts(usr_id, 0)
+        db_chng.change_word(usr_id, word)
+        print('Успех')
+
+    bot.reply_to(message, 'Я загадал, угадывай')
     if usr_id in admins:
         bot.reply_to(message, str(word))
     # bot.register_next_step_handler(message, result_back)
@@ -37,8 +48,7 @@ def find(message):
 def answer(message):
     usr_id = str(message.from_user.id)
     # проверка, есть ли юзер в базе
-    user = db_req.req_user(usr_id)
-    print(user)
+    
 
 
 if __name__ == '__main__':
